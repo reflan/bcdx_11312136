9ffc777fd897fcc345f2a979e4b74c9100ae5928
reflan nuari <reflannuari@gmail.com>
2023-11-15 19:14:07 +0700

    commit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bcdx_11312136/app/controllers/auth_controller.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,13 +6,19 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  final cAuth = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => cAuth.logout(),
+            icon: Icon(Icons.logout),
+          )
+        ],
       ),
       body: const Center(
         child: Text(
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
+import 'package:bcdx_11312136/app/controllers/auth_controller.dart';
+import 'package:bcdx_11312136/app/utils/loading.dart';
+import 'package:bcdx_11312136/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final CAuth = Get.put(AuthController(), permanent: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: CAuth.streamAuthStatus,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          return GetMaterialApp(
+            title: "Application",
+            initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+            getPages: AppPages.routes,
+          );
+        }
+        return Loading();
+      },
+    );
+  }
 }
