31cef493863e0f3d1de483fc2e7a5798c10e2292
reflan nuari <reflannuari@gmail.com>
2023-11-29 14:52:48 +0700

    commit

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:bcdx_11312136/app/controllers/auth_controller.dart';
 import 'package:bcdx_11312136/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
@@ -38,6 +40,16 @@ class LoginView extends GetView<LoginController> {
               onPressed: () =>
                   cAuth.login(controller.cEmail.text, controller.cPass.text),
               child: Text("Login"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: ()=> Get.toNamed(Routes.RESET_PASSWORD),
+                child: Text("Reset Password"),
+              ),
             ),
             SizedBox(
               height: 10,
